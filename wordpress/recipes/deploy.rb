8cb8abd5c5c5a018ec29b83e0c60f17383e085bf
Paul Brough <paul.brough@paulsenmarketing.com>
2014-01-14 15:17:14 -0600

    asdf

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -27,23 +27,24 @@ node[:deploy].each do |app_name, deploy|
 
     if defined?node[:deploy][:writableDirs]
       node[:deploy][:writableDirs].each do |dir_name|
+        Chef::Log.info("Paulsen Wordpress - CHOWN #{dir_name}")
 
-        if File.directory?("#{deploy[:deploy_to]}/current")
+        # if File.directory?("#{deploy[:deploy_to]}/current")
 
-            if File.exist? "#{deploy[:deploy_to]}/current/#{dir_name}"
-                Chef::Log.info("Paulsen Wordpress - chowning #{dir_name}")
-                script "set_permissions_wp-content" do
-                  interpreter "bash"
-                  user "root"
-                  cwd "#{deploy[:deploy_to]}/current"
-                  code <<-EOH
-                  sudo chown -R apache #{dir_name}
-                  EOH
-                end
-                Chef::Log.info("Paulsen Wordpress - done chowning #{dir_name}")
-            end
+        #     if File.exist? "#{deploy[:deploy_to]}/current/#{dir_name}"
+        #         Chef::Log.info("Paulsen Wordpress - chowning #{dir_name}")
+        #         script "set_permissions_wp-content" do
+        #           interpreter "bash"
+        #           user "root"
+        #           cwd "#{deploy[:deploy_to]}/current"
+        #           code <<-EOH
+        #           sudo chown -R apache #{dir_name}
+        #           EOH
+        #         end
+        #         Chef::Log.info("Paulsen Wordpress - done chowning #{dir_name}")
+        #     end
 
-        end
+        # end
 
       end
     else
