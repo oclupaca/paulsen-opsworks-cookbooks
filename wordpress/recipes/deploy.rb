355f44bc061cf8ed375285edd1fba473a4af7a0c
Paul Brough <paul.brough@paulsenmarketing.com>
2014-01-15 08:54:22 -0600

    add dir chowning back in

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -13,46 +13,75 @@ keys = response.body
 node[:deploy].each do |app_name, deploy|
 
   template "#{deploy[:deploy_to]}/current/composer.json" do
-      source "composer.json.erb"
-      mode 0660
-      group deploy[:group]
+    source "composer.json.erb"
+    mode 0660
+    group deploy[:group]
 
-      if platform?("ubuntu")
-        owner "www-data"
-      elsif platform?("amazon")
-        owner "apache"
-      end
+    if platform?("ubuntu")
+      owner "www-data"
+    elsif platform?("amazon")
+      owner "apache"
+    end
   end
 
   script "install_composer" do
-      interpreter "bash"
-      user "root"
-      cwd "#{deploy[:deploy_to]}/current"
-      code <<-EOH
-      curl -s https://getcomposer.org/installer | php
-      php composer.phar install
-      EOH
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    curl -s https://getcomposer.org/installer | php
+    php composer.phar install
+    EOH
   end
 
-    template "#{deploy[:deploy_to]}/current/aws-config.php" do
-        Chef::Log.info('Paulsen Wordpress - creating aws-config ')
-        source "aws-config.php.erb"
-        mode 0660
-        group deploy[:group]
+  template "#{deploy[:deploy_to]}/current/aws-config.php" do
+    Chef::Log.info('Paulsen Wordpress - creating aws-config ')
+    source "aws-config.php.erb"
+    mode 0660
+    group deploy[:group]
 
-        if platform?("ubuntu")
-          owner "www-data"
-        elsif platform?("amazon")
-          owner "apache"
-        end
+    if platform?("ubuntu")
+      owner "www-data"
+    elsif platform?("amazon")
+      owner "apache"
+    end
 
-        variables(
+    variables(
             # :database   => (deploy[:database][:database] rescue nil),
             :database   => (app_name rescue nil),
             :user       => (node[:rds][:username] rescue nil),
             :password   => (node[:rds][:password] rescue nil),
             :host       => (node[:rds][:endpoint] rescue nil)
-        )
+            )
+  end
+
+  myDirs = deploy[:writableDirs]
+
+  if defined?myDirs
+    if myDirs.kind_of?(Array)
+      Chef::Log.info("Paulsen Wordpress - wridableDirs.length is #{deploy[:writableDirs].length}");
+      myDirs.each do |dir_name|
+        Chef::Log.info("Paulsen Wordpress - CHOWN #{dir_name}")
+
+        if File.directory?("#{deploy[:deploy_to]}/current")
+
+            if File.exist? "#{deploy[:deploy_to]}/current/#{dir_name}"
+                Chef::Log.info("Paulsen Wordpress - chowning #{deploy[:deploy_to]}/current/#{dir_name}")
+                script "set_permissions_wp-content" do
+                  interpreter "bash"
+                  user "root"
+                  cwd "#{deploy[:deploy_to]}/current"
+                  code <<-EOH
+                  sudo chown -R apache #{deploy[:deploy_to]}/current/#{dir_name}
+                  EOH
+                end
+                Chef::Log.info("Paulsen Wordpress - done chowning #{deploy[:deploy_to]}/current/#{dir_name}")
+            end
+
+        end
+
+      end
     end
+  end
 
 end
